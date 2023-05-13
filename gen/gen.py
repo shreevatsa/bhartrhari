a6d95125297caeac0996357c140e14a7ebf50a47
Shreevatsa R <shreevatsa.public@gmail.com>
2023-05-12 20:23:43 -0700

    Add Kosambi and fix Gopinath images

--- gen/gen.py
+++ gen/gen.py
@@ -45,11 +45,11 @@ with open('../data/regions/telang-regions-out.json') as file:
     totHeight = t['totHeight']
     imageUrlPrefixes['Telang'] = imageUrlPrefix = t['imageUrlPrefix'] + '_202305'
     pageUrlPrefixes['Telang'] = pageUrlPrefix = t['pageUrlPrefix'] + '_202305'
-    for (name, types_and_regions) in t['regions']:
-        telang[name] = collections.defaultdict(list)
+    for (region_name, types_and_regions) in t['regions']:
+        telang[region_name] = collections.defaultdict(list)
         for (type, regions) in types_and_regions.items():
             for region in regions:
-                telang[name][type].append({
+                telang[region_name][type].append({
                     'n': region['slug'] - 1,
                     'x': int(region['xmin'] / totWidth * 100) / 100,
                     'y': int(region['ymin'] / totHeight * 1000) / 1000,
@@ -57,6 +57,27 @@ with open('../data/regions/telang-regions-out.json') as file:
                     'h': (int(region['height'] / totHeight * 1000) + 5) / 1000,
                     'text': region['text'],
                 })
+kosambi = {}
+with open('../data/regions/kosambi-regions-out.json') as file:
+    t = json.load(file)
+    totWidth = t['totWidth']
+    totHeight = t['totHeight']
+    imageUrlPrefixes['Kosambi'] = imageUrlPrefix = t['imageUrlPrefix']
+    pageUrlPrefixes['Kosambi'] = pageUrlPrefix = t['pageUrlPrefix']
+    for (region_name, types_and_regions) in t['regions']:
+        region_name = 'K' + region_name
+        kosambi[region_name] = collections.defaultdict(list)
+        for (type, regions) in types_and_regions.items():
+            for region in regions:
+                kosambi[region_name][type].append({
+                    'n': region['page_id'] - 1,
+                    'x': int(region['xmin'] / totWidth * 100) / 100,
+                    'y': int(region['ymin'] / totHeight * 1000) / 1000,
+                    'w': (int(region['width'] / totWidth * 100) + 2) / 100,
+                    'h': (int(region['height'] / totHeight * 1000) + 5) / 1000,
+                    'text': region['text'],
+                })
+print(kosambi.keys())
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape, StrictUndefined
 # Create a custom Jinja2 environment
@@ -70,17 +91,25 @@ for (k, vs) in versions.items():
     k = f'K{int(k):03}'
     versions_for_template = []
     kOrder = ['Ryder', 'Tawney', 'Mādhavānanda', 'Telang', 'Gopinath1914', 'Gopinath1896']
-    for (name, versions_by_book) in sorted(vs.items(), key=lambda name_and_v: kOrder.index(name_and_v[0])):
+    for (book_name, versions_by_book) in sorted(vs.items(), key=lambda name_and_v: kOrder.index(name_and_v[0])):
         for version in versions_by_book:
-            if name == 'Telang':
+            if book_name == 'Telang':
                 region_name = version.strip()
                 versions_for_template.append({
-                    'title': name,
+                    'title': book_name,
                     'regions': telang[region_name],
-                    'pageUrlPrefix': pageUrlPrefixes[name],
-                    'imageUrlPrefix': imageUrlPrefixes[name],
+                    'pageUrlPrefix': pageUrlPrefixes[book_name],
+                    'imageUrlPrefix': imageUrlPrefixes[book_name],
                 })
                 continue
+            if book_name in ['Gopinath1914', 'Gopinath1896']:
+                region_name = version.strip()
+                versions_for_template.append({
+                    'title': book_name,
+                    'image_urls': ['https://calm-gaufre-6360e8.netlify.app/data/images/' + version],
+                })
+                continue
+
             # Ignore leading and trailing blank lines
             lines = version.splitlines()
             while lines and lines[0].strip() == '': lines = lines[1:]
@@ -103,9 +132,17 @@ for (k, vs) in versions.items():
                     'text': line,
                 })
             versions_for_template.append({
-                'title': name,
+                'title': book_name,
                 'lines': lines_for_template,
             })
+    if k in kosambi:
+        region_name = k
+        versions_for_template.append({
+            'title': 'Kosambi',
+            'regions': kosambi[region_name],
+            'pageUrlPrefix': pageUrlPrefixes['Kosambi'],
+            'imageUrlPrefix': imageUrlPrefixes['Kosambi'],
+        })
     # Render the template with data
     template = env.get_template('template.html')
     output = template.render(
